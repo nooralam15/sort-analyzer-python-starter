73200c5ffe9c30a4b8c4ce42df378ccf35936cc1
nooralam15 <nooralam14@icloud.com>
2022-03-10 14:52:24 -0700

    Update sort-analyzer.py

--- sort-analyzer.py
+++ sort-analyzer.py
@@ -2,6 +2,52 @@
 
 import time
 
+
+#Initialize a function that will use bubble sort 
+def bubbleSort(anArray):
+    for numCompare in range(len(anArray)-1, 0, -1):
+        for i in range(numCompare):
+            if anArray[i] > anArray[i + 1]:
+                temp = anArray[i]
+                anArray[i] = anArray[i + 1]
+                anArray[i + 1] = temp
+
+
+#Initliaze a function that will use selection sort 
+def selectionSort(anArray):
+    for fillSlot in range(0, len(anArray)-1):
+        #Search for minimum
+        minPosition = fillSlot
+        for i in range(fillSlot +1 , len(anArray)):
+            if anArray[i] < anArray[minPosition]:
+                minPosition = i
+       
+        temp = anArray[fillSlot]
+        anArray[fillSlot] = anArray[minPosition]
+        anArray[minPosition] = temp
+
+
+#Initliaze a function that uses insertion sort 
+def insertionSort(anArray):
+    for i in range(1, len(anArray)):
+        insertVal = anArray[i]
+        insertPos = i
+
+        while insertPos > 0 and anArray[insertPos - 1] > insertVal:
+            anArray[insertPos] = anArray[insertPos-1]
+            insertPos-=1
+
+        anArray[insertPos] = insertVal
+
+
+#Initialize a function that will time each sort algorithm
+def sortTimer(searchFunction, array, arrayName, functionName):
+    startTime = time.perf_counter()
+    searchFunction(array)
+    endTime = time.perf_counter()
+    print(arrayName + " took " + str(endTime - startTime) + " seconds in " + functionName)
+
+
 # RETURN DATA FROM FILE AS AN ARRAY OF INTERGERS
 def loadDataArray(fileName):
     temp = []
@@ -23,15 +69,19 @@ reversedData = loadDataArray("data-files/reversed-values.txt")
 nearlySortedData = loadDataArray("data-files/nearly-sorted-values.txt")
 fewUniqueData = loadDataArray("data-files/few-unique-values.txt")
 
+
+#Call the functions
+sortTimer(bubbleSort, randomData, "randomData", "bubbleSort")
+sortTimer(selectionSort, randomData, "randomData", "selectionSort")
+sortTimer(insertionSort, randomData, "randomData", "insertionSort")
+
+sortTimer(bubbleSort, reversedData, "reversedData", "bubbleSort")
+sortTimer(selectionSort, reversedData, "reversedData", "selectionSort")
+sortTimer(insertionSort, reversedData, "reversedData", "insertionSort")
+
 # VERIFY LOADED DATA BY PRINTING FIRST 50 ELEMENTS
-print(randomData[0:50])
-print(reversedData[0:50])
-print(nearlySortedData[0:50])
-print(fewUniqueData[0:50])
+# print(randomData[0:50])
+# print(reversedData[0:50])
+# print(nearlySortedData[0:50])
+# print(fewUniqueData[0:50])
 
-
-# EXAMPLE OF HOW TO TIME DURATION OF A SORT ALGORITHM
-# startTime = time.time()
-# bubbleSort(randomData)
-# endTime = time.time()
-# print(f"Bubble Sort Random Data: {endTime - startTime} seconds")
